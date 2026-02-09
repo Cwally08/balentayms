91e47d69514de647abed4e432aadc1e49d9f3f27
Cwally <christianwallyatendido@gmail.com>
2026-02-09 09:09:26 +0800

    fix

--- js/game.js
+++ js/game.js
@@ -31,7 +31,7 @@ const puzzles = [
         word: "FUTURE",
         letters: ["F","H","T","B","R","E","A","U","C","D","G","U"],
         images: [
-            "../images/future1.webp",
+            "../images/future1.jpg",
             "../images/future3.jpg",
             "../images/future4.jpg",
             "../images/future2.jpg"
